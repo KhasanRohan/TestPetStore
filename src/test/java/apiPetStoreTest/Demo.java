0969d05d57699a2da6a39d75d6b229eb052381fb
KhasanRohan <g.nurudinov@mail.ru>
2024-03-01 21:08:15 +0300

    init commit

---
+++ src/test/java/apiPetStoreTest/Demo.java
@@ -0,0 +1,14 @@
+package apiPetStoreTest;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Demo {
+    @Test
+    void demo() {
+       open("https://wiremock.org/docs/stubbing/");
+
+        }
+    }
+

3c7b18251a1faee80ffbf44ba7eb45bb6861f0f3
KhasanRohan <g.nurudinov@mail.ru>
2024-03-01 21:10:19 +0300

    added sleep

--- src/test/java/apiPetStoreTest/Demo.java
+++ src/test/java/apiPetStoreTest/Demo.java
@@ -3,12 +3,13 @@ package apiPetStoreTest;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.sleep;
 
 public class Demo {
     @Test
     void demo() {
        open("https://wiremock.org/docs/stubbing/");
-
+       sleep(5000);
         }
-    }
+}
 

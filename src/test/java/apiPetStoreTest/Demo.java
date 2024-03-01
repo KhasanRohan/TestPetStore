97ca09781e6e1ef76158bfa8389b46eabe8529ea
KhasanRohan <g.nurudinov@mail.ru>
2024-03-01 21:29:23 +0300

    added plus

--- src/test/java/apiPetStoreTest/Demo.java
+++ src/test/java/apiPetStoreTest/Demo.java
@@ -8,7 +8,10 @@ public class Demo {
     @Test
     void demo() {
        open("https://wiremock.org/docs/stubbing/");
-
+       int a = 1;
+       int b = 2;
+       int result = a + b;
+        System.out.println(result);
         }
     }
 
